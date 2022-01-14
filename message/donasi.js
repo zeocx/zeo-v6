6c88b20b4f61d3f45e50f5c00e0bdf2b114c5932
zeocx <gilangabied@gmail.com>
2022-01-14 23:01:05 +0700

    nabilla

---
+++ message/donasi.js
@@ -0,0 +1,14 @@
+exports.dodo = (prefix) => {
+return`
+╭────────❲ *DONASI* ❳
+│
+│  Bantu Developer ngembangin bot ini
+│  dengan donasi seiklasnya 🙂
+│
+│  DANA : 089516207932
+│ 
+│ Makasih yang udah donasi
+│ dan bantu ngembangin bot ini
+│
+╰────────⏣`
+}
